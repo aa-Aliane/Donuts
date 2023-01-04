6cd94b8ec87cce54159afcf66942a84e89c65cec
aaliane781@gmail.com <aaliane781@gmail.com>
2023-01-04 17:55:14 +0100

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+  
+    <div className="main__container">
+        <header className='header'>
+            <h1>DonuT</h1>
+        </header>
+    </div>
+
+  )
+}
+
+export default Home
