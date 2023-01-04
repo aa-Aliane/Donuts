fd2d67ee67d5b30ca0cb6a7c45b394821e571329
aaliane781@gmail.com <aaliane781@gmail.com>
2023-01-04 18:05:21 +0100

    Update _home.scss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-  
     <div className="main__container">
-        <header className='header'>
-            <h1>DonuT</h1>
-        </header>
+      <nav className="nav">
+        <h2 className="nav__logo">DonuT</h2>
+        <span class="material-symbols-outlined">menu</span>
+      </nav>
     </div>
-
-  )
+  );
 }
 
 export default Home
