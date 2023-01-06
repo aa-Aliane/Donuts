833e99636583ae6da41c2e500eee15b59a003d50
aaliane781@gmail.com <aaliane781@gmail.com>
2023-01-06 13:04:08 +0100

    Add fifth section

--- src/components/HomeS5.jsx
+++ src/components/HomeS5.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export const HomeS5 = () => {
   return (
-    <div>HomeS5</div>
-  )
-}
+    <div className="home-s5">
+      <div className="home-s5__section">
+        <h4>recipes</h4>
+        <p>
+          for a beginner baker, donuts can be frightening, but it doesn't have
+          to be. We combine a few of our go-to donuts recipes each month.
+        </p>
+      </div>
+      <div className="home-s5__section">
+        <h4>community</h4>
+        <p>
+          searching for a location to hold an event? All types of creative
+          people are welcome at DONUT.
+        </p>
+      </div>
+      <div className="home-s5__img">
+        <img src="src/assets/homes5.png" alt="#" />
+      </div>
+    </div>
+  );
+};
