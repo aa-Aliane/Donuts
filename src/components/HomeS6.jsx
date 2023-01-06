913481f188dc9ff5c59513bf7233f0cc1553e76c
aaliane781@gmail.com <aaliane781@gmail.com>
2023-01-06 14:00:51 +0100

    Update HomeS6.jsx

--- src/components/HomeS6.jsx
+++ src/components/HomeS6.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export const HomeS6 = () => {
   return (
-    <div>HomeS6</div>
-  )
-}
+    <div className="home-s6 dark">
+      <img src="src/assets/hs61.png" alt="#" />
+      <img src="src/assets/hs62.png" alt="#" />
+      <img src="src/assets/hs63.png" alt="#" />
+      <img src="src/assets/hs64.png" alt="#" />
+      <img src="src/assets/hs65.png" alt="#" />
+      <img src="src/assets/hs66.png" alt="#" />
+      <div className="home-s6__social">follow us on Instagram @DonuT</div>
+    </div>
+  );
+};
